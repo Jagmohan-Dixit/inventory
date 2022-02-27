0bc3f49c7c4fbd860ed05521ce2752c9b678e62a
Tanvi Naik <naiktanvi30@gmail.com>
2022-02-27 23:22:33 +0530

    jsondata added

--- db.py
+++ db.py
@@ -13,7 +13,6 @@ conn.execute('DROP TABLE IF EXISTS district')
 
 
 
-
 # conn.execute('CREATE TABLE inventory (issuedfrom STRING, productname STRING, date STRING, dateofsurvey STRING, billno STRING, nameoffirm STRING, itemno STRING, quantity STRING, rateperitem STRING, totalamount STRING, crvno STRING)')
 # conn.execute('CREATE TABLE issued (issuedfrom STRING, productname STRING, issuedto STRING, district STRING, quantity STRING)')
 conn.execute('CREATE TABLE logindata (email STRING, password STRING)')
@@ -25,7 +24,6 @@ conn.execute('CREATE TABLE logindata (email STRING, password STRING)')
 ########### new #########
 conn.execute('''CREATE TABLE inventory 
     (
-    productId INTEGER PRIMARY KEY AUTOINCREMENT,
     issuedfrom STRING, 
     productname STRING, 
     date STRING, 
@@ -39,27 +37,27 @@ conn.execute('''CREATE TABLE inventory
     crvno STRING UNIQUE)''')
 
 
-conn.execute('''CREATE TABLE district (
-    districtId INTEGER PRIMARY KEY AUTOINCREMENT,
-    name STRING)''')
-
-conn.execute('''CREATE TABLE battalions (
-    battalionId INTEGER PRIMARY KEY AUTOINCREMENT,
-    name STRING,
-    districtId INTEGER references district(districtId))''')
-
-conn.execute('''CREATE TABLE policestation (
-    psId INTEGER PRIMARY KEY AUTOINCREMENT,
-    psname STRING,
-    districtId INTEGER references district(districtId))''')
+# conn.execute('''CREATE TABLE district (
+#     districtId INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name STRING)''')
+#
+# conn.execute('''CREATE TABLE battalions (
+#     battalionId INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name STRING,
+#     districtId INTEGER references district(districtId))''')
+#
+# conn.execute('''CREATE TABLE policestation (
+#     psId INTEGER PRIMARY KEY AUTOINCREMENT,
+#     psname STRING,
+#     districtId INTEGER references district(districtId))''')
 
 conn.execute('''CREATE TABLE issuedTo 
     (issuedfrom STRING, 
-    productname STRING references inventory(productId), 
-    battalionId INTEGER references battalions(battalionId), 
-    districtId INTEGER references district(districtId),
-    policestation INTEGER references policestation(psId),
-    quantity INTEGER)''')
+    productname STRING ,
+    battalion  STRING,
+    district  STRING,
+    policestation STRING
+    quantity STRING)''')
 
 
 
@@ -71,34 +69,34 @@ cur.execute('INSERT INTO logindata (email, password) VALUES (?,?)', ("naiktanvi3
 
 
 
-districts = [('Shimla', ),
-             ('Solan',),
-             ('Police Distt. Baddi',),
-             ('Sirmaur',),
-             ('Kinnaur',),
-             ('Bilaspur',),
-             ('Mandi',),
-             ('Kullu',),
-             ('Lahaul & Spiti',),
-             ('Hamirpur',),
-             ('Chamba',),
-             ('Kangra',),
-             ('Una',),
-             ('Traffic, Tourist and Railway',),
-             ('State CID',),
-]
-
-battalions = [('HPABN Junga', 1),
-                ('1st IRBN Bangarh', 13),
-                ('2nd IRBN Sakoh', 12),
-                ('3rd IRBN Pandoh', 7),
-                ('4th IRBN Jangal Beri', 10),
-                ('5th IRBN Bassi', 6),
-                ('6th IRBN Kolar', 4),
-              ]
-
-cur.executemany('INSERT INTO district (name) VALUES (?)', districts)
-cur.executemany('INSERT INTO battalions (name, districtId) VALUES (?,?)', battalions)
+# districts = [('Shimla', ),
+#              ('Solan',),
+#              ('Police Distt. Baddi',),
+#              ('Sirmaur',),
+#              ('Kinnaur',),
+#              ('Bilaspur',),
+#              ('Mandi',),
+#              ('Kullu',),
+#              ('Lahaul & Spiti',),
+#              ('Hamirpur',),
+#              ('Chamba',),
+#              ('Kangra',),
+#              ('Una',),
+#              ('Traffic, Tourist and Railway',),
+#              ('State CID',),
+# ]
+#
+# battalions = [('HPABN Junga', 1),
+#                 ('1st IRBN Bangarh', 13),
+#                 ('2nd IRBN Sakoh', 12),
+#                 ('3rd IRBN Pandoh', 7),
+#                 ('4th IRBN Jangal Beri', 10),
+#                 ('5th IRBN Bassi', 6),
+#                 ('6th IRBN Kolar', 4),
+#               ]
+#
+# cur.executemany('INSERT INTO district (name) VALUES (?)', districts)
+# cur.executemany('INSERT INTO battalions (name, districtId) VALUES (?,?)', battalions)
 
 # cur.execute('INSERT INTO policestation (psname, district) VALUES (?,?)',('P.S. Sadar ',1))
 # cur.execute('INSERT INTO vs (psname, district) VALUES (?,?)',('P.S. East', 'Shimla'))
